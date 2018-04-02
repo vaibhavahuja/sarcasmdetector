bd56a394bf7e0e8bef66735bfc0e719f5a519c5d
Vaibhav Ahuja <vaibhavahuja@outlook.com>
2018-04-03 03:27:47 +0530

    changed the preprocessing file.

---
+++ scripts/preprocessing.py
@@ -0,0 +1,17 @@
+import re
+import io
+
+def preprocess(tweet):
+    final = []
+    if('http' not in tweet and r'\u' not in tweet):
+        removingNonAlphabets = re.compile('[^a-zA-Z ]')
+        hashtags = re.compile(r'#\w*\s?')
+        tags = re.compile(r'@\w*\s?')
+        tweet = re.sub(hashtags,'',tweet)
+        tweet = re.sub(tags,'', tweet)
+        tweet = re.sub(removingNonAlphabets,'',tweet)
+        final = [tweet,len(tweet.split())]
+    else:
+        tweet=""
+        final = [tweet,len(tweet.split())]
+    return final
